a9b507f2b55b32e364e0463bfe4d26c2f3086683
Mdinh22 <mdinh22@student.cccd.edu>
2020-09-08 12:37:35 -0700

    Update redditDataset.py

--- redditDataset.py
+++ redditDataset.py
@@ -1,10 +1,14 @@
 # Imports PRAW. If you do not have PRAW installed, please refer to the FAQ
 # (to be included when giving to another student)
+# If you use PyCharm, type in terminal:
+# pip install praw
+# pip install --upgrade praw
 import praw
 
 # Authentication to Reddit's servers using a Reddit account.
 reddit = praw.Reddit(client_id='zSSSh_xVCB2CQg',
-                     client_secret='zDlIP1ENlrX08Xlqu_l4FQgfujc', password='orangecoast22',
+                     client_secret='zDlIP1ENlrX08Xlqu_l4FQgfujc',
+                     password='orangecoast22',
                      user_agent='Python Test App', username='cs131praw')
 
 # Obtain an instance of the subreddit object from the API wrapper.
@@ -26,15 +30,17 @@ class redditPost:
         self.ratio = submission.upvote_ratio
         self.comments = submission.num_comments
         self.date = submission.created_utc
-
-# Filtering certain posts. In this case, we filter out stickied posts, since they are always
-# on top. If a post passes the filter, create an instance of our class and add it to our list
+# Filtering certain posts. In this case, we filter out stickied posts,
+# since they are always on top.
+# If a post passes the filter, create an instance of our class and
+# add it to our list
 for submission in hot_python:
     if submission.stickied == 0:
         posts.append(redditPost())
 
 
-# For each post in our list, print out its relevant attributes in string format.
+# For each post in our list,
+# print out its relevant attributes in string format.
 for i in posts:
     print(i.title)
     print(i.score)
@@ -47,4 +53,3 @@ for i in posts:
     subredditTuple = (i.title, i.score, i.ratio,
                       i.date, i.comments)
     print(subredditTuple)
-
